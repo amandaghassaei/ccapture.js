e5f59c74f88ba8b7f2bda800c10a66554a3e46d0
Amanda Ghassaei <amandaghassaei@gmail.com>
2021-01-28 22:36:22 -0800

    fixing workers import

--- src/CCapture.js
+++ src/CCapture.js
@@ -5,7 +5,6 @@ if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
   var download = require('./download.js');
   var GIF = require('./gif.js');
   var WebMWriter = require('./webm-writer-0.2.0');
-  var gifWorker = require('./gif.worker.js'); // import this so that it gets packaged with the rest of the code.
 }
 
 "use strict";
@@ -507,7 +506,6 @@ function CCGIFEncoder( settings ) {
 	CCFrameEncoder.call( this, settings );
 
 	settings.quality = 31 - ( ( settings.quality * 30 / 100 ) || 10 );
-	settings.
 	s = settings.workers || 4;
 
 	this.extension = '.gif'
@@ -520,7 +518,7 @@ function CCGIFEncoder( settings ) {
   	this.encoder = new GIF({
 		workers: settings.workers,
 		quality: settings.quality,
-		workerScript: 'gif.worker.js'
+		workerScript: settings.workersPath,
 	} );
 
     this.encoder.on( 'progress', function( progress ) {
